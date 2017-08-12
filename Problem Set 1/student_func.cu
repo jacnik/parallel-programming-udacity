31a9ac3aaff82321a60457adfbc82fd5c1698130
Jacek Kubicki <jacnik@gmail.com>
2017-08-12 14:22:01 +0200

    HW1 first attempt, few random pixels difference between ref

--- Problem Set 1/student_func.cu
+++ Problem Set 1/student_func.cu
@@ -50,6 +50,12 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+ 	int row = threadIdx.x; //column
+	int column = blockIdx.x; //row
+	
+	int offset = column + row * numCols;
+	uchar4 rgba = rgbaImage[offset];
+	greyImage[offset] = rgba.x * 0.299f + rgba.y * 0.587f + rgba.z * 0.114f;
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -57,8 +63,10 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+  
+  //todo check if numCols > 1024
+  const dim3 blockSize(numRows, 1, 1);  //TODO
+  const dim3 gridSize(numCols, 1, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
